8f1f1e5053b27143958ef5fa15eaa6482ae5d3d9
nlnl03 <lavinoam2003@gmail.com>
2024-02-13 00:03:36 +0200

    new

--- server/server.js
+++ server/server.js
@@ -24,24 +24,41 @@ app.get("/api/v1/restaurants", async (req, res) => {
  });
 
  // get a restaurants
-app.get("/api/v1/restaurants/:id", (req,res)=>{
-    console.log(req.params)
-    res.status(200).json({
-        status: "success",
-        data:{
-            restaurant: "mcdonalds"
-        }
-    })
+app.get("/api/v1/restaurants/:id", async (req,res)=>{
+    try{
+        const results = await db.query("select * from restaurants where id = $1", [req.params.id]);
+ 
+        res.status(200).json({
+            status: "success",
+            data:{
+                restaurant: results.rows[0]
+            }
+        })
+    }catch(err){
+        console.log(err)
+    }
+     
 })
  //post
- app.post("/api/v1/restaurants", (req,res) => {
-    console.log(req.body)
-    res.status(201).json({
-        status: "success",
-        data:{
-            restaurant: "mcdonalds"
-        }
-    })
+ app.post("/api/v1/restaurants",async (req,res) => {
+    console.log(req.body);
+
+    try{
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *", 
+            [req.body.name, req.body.location, req.body.price_range]
+        )
+        console.log(results)
+
+        res.status(201).json({
+            status: "success",
+            data:{
+                restaurant: results.rows[0]
+            }
+        })
+    
+    }catch(err){
+        console.log(err)
+    }
  });
 
 //update
